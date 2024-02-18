20046a5ae7888d6068a6654c42c957d3aaab25ea
JoshuaR26 <joshua2009474@gmail.com>
2024-02-18 10:17:04 +0530

    smnthtg

---
+++ JupyterNB/tempCodeRunnerFile.py
@@ -0,0 +1,6 @@
+if 'Attendance.csv' in os.listdir(os.path.join(os.getcwd(),'attendace')):
+#     print("there iss..")
+#     # os.remove("Attendance.csv")
+# else:
+#     df=pd.DataFrame(list())
+#     # df.to_csv("Attendance.csv")
